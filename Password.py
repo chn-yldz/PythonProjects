9d4f5276bd024f5bf1ef7ddbf984e3bc0030c16e
Cihan Yıldız <69516827+chn-yldz@users.noreply.github.com>
2021-01-14 01:48:03 +0300

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,26 @@
+"""
+Random password generation
+"""
+import time
+import random
+from tqdm import tqdm
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{},.-*&+-/?!;"
+
+all = lower+upper+numbers+symbols
+while True:
+   print("1)Create a password\n2)Quit")
+   choice = int(input("Choice :"))
+   if choice == 2:
+       time.sleep(1)
+       break
+   elif choice == 1:
+        length = int(input("Password length :"))
+        for i in tqdm(range(10)):
+            time.sleep(.4)
+        password = "".join(random.sample(all,length))
+        print("***PASSWORD***")
+        print(password)
+        time.sleep(2)
